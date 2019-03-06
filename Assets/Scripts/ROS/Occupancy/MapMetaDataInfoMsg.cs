8bde9c4eaa1e07aa206cedd59d845dfefc37e3b9
Eric <ejyzhang@berkeley.edu>
2019-03-05 18:37:06 -0800

    Moved things into folders and added occupancy map display

---
+++ Assets/Scripts/ROS/Occupancy/MapMetaDataInfoMsg.cs
@@ -0,0 +1,55 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.geometry_msgs;
+using UnityEngine;
+using SimpleJSON;
+
+namespace ROSBridgeLib
+{
+    namespace cartographer_msgs
+    {
+        public class MapMetaDataInfoMsg : ROSBridgeMsg
+        {
+            private string _time;
+            private float _resolution;
+            private int _width;
+            private int _height;
+            private PoseMsg _origin;
+
+            public MapMetaDataInfoMsg(JSONNode node)
+            {
+                _time = node["map_load_time"];
+                _resolution = node["resolution"].AsFloat;
+                _width = node["width"].AsInt;
+                _height = node["height"].AsInt;
+                _origin = new PoseMsg(node["origin"]);
+            }
+
+
+            public string GetTime()
+            {
+                return _time;
+            }
+
+            public float GetResolution()
+            {
+                return _resolution;
+            }
+
+            public int GetWidth()
+            {
+                return _width;
+            }
+
+            public int GetHeight()
+            {
+                return _height;
+            }
+
+            public PoseMsg GetOrigin()
+            {
+                return _origin;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/ROS/Occupancy/OccupancyGridMsg.cs
@@ -0,0 +1,44 @@
+﻿using SimpleJSON;
+using ROSBridgeLib.std_msgs;
+using UnityEngine;
+
+namespace ROSBridgeLib
+{
+    namespace cartographer_msgs
+    {
+        public class OccupancyGridMsg : ROSBridgeMsg
+        {
+            private HeaderMsg _header;
+            private MapMetaDataInfoMsg _info;
+            private byte[] _data;
+
+            public OccupancyGridMsg(JSONNode node)
+            {
+                _header = new HeaderMsg(node["header"]);
+                _info = new MapMetaDataInfoMsg(node["info"]);
+                JSONArray originalData = node["data"].AsArray;
+                _data = new byte[originalData.Count];
+                for (int i = 0; i < _data.Length; i++)
+                {
+                    _data[i] = (byte)originalData[i].AsInt;
+                }
+            }
+
+            
+            public HeaderMsg GetHeader()
+            {
+                return _header;
+            }
+
+            public MapMetaDataInfoMsg GetInfo()
+            {
+                return _info;
+            }
+
+            public byte[] GetData()
+            {
+                return _data;
+            }
+        }
+    }
+}
