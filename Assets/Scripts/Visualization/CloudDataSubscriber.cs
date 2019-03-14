31262318c2689e11214a7a5e5c37a896abd96388
Eric <ejyzhang@berkeley.edu>
2019-03-14 15:04:31 -0700

    Merged data subscriber code

---
+++ Assets/Scripts/Visualization/CloudDataSubscriber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class CloudDataSubscriber : MonoBehaviour, DataServer.DataSubscriber
+{
+    public DataServer server;
+
+    void Start ()
+    {
+        server.RegisterDataSubscriber("Cloud", this);
+    }
+
+    public void OnReceiveMessage(float timestamp, string message)
+    {
+        string[] parts = message.Split(',');
+        float x, y, z;
+
+        if (parts.Length >= 3 && float.TryParse(parts[0], out x) && 
+            float.TryParse(parts[1], out y) && float.TryParse(parts[2], out z))
+        {
+
+        }
+    }
+}
